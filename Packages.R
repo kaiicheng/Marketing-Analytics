7d734b642e507773b21b34bfd7b5fe6cdafd11cd
Kai <33795285+kaiicheng@users.noreply.github.com>
2020-11-25 23:38:59 +0800

    Add files via upload

---
+++ Packages.R
@@ -0,0 +1,9 @@
+# Install packages.
+install.packages('gdata')
+
+# Load library and suppresses unnecessary output.
+suppressMessages(library("gdata"))  
+library("ggplot2")
+library("ggthemes")
+library("scales")
+suppressMessages(library("arm")) 
